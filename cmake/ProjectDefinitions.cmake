cb9272b63c18ddecfbc3d06889b13d2a004cfe6c
Tatyana Borisova <tanusshhka@mail.ru>
2019-03-15 21:18:07 +0300

    Devide Unix and Windows flag setting

--- cmake/ProjectDefinitions.cmake
+++ cmake/ProjectDefinitions.cmake
@@ -1,4 +1,8 @@
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-set(CMAKE_CXX_FLAGS "-Wall -Werror")
+if(UNIX)
+    set(CMAKE_CXX_FLAGS "-Wall -Werror")
+elseif(WIN32)
+    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-Wall" "-Werror")
+endif()
