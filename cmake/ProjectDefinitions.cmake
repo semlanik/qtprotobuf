10e4ce59a0b6f8ad79d01e84360ec187c0ab1b1d
Viktor Kopp <vifactor@gmail.com>
2019-03-21 17:40:52 +0100

    Additional cleaning of CMakeLists.txt

--- cmake/ProjectDefinitions.cmake
+++
@@ -1,8 +0,0 @@
-set(CMAKE_CXX_STANDARD 14)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
-if(UNIX)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror")
-elseif(WIN32)
-    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-Wall" "-Werror")
-endif()
