ed349cb46c04beb536a58687138169dce990321d
Alexey Edelev <semlanik@gmail.com>
2019-12-16 19:44:08 +0100

    Make return value of simple types getters constant
    Fixed: #14

--- src/generator/templates.cpp
+++ src/generator/templates.cpp
@@ -141,7 +141,7 @@ const char *Templates::GetterMessageDefinitionTemplate = "const $type$ &$classna
                                         "    return *m_$property_name$;\n"
                                         "}\n\n";
 
-const char *Templates::GetterTemplate = "$type$ $property_name$() const {\n"
+const char *Templates::GetterTemplate = "const $type$ $property_name$() const {\n"
                                         "    return m_$property_name$;\n"
                                         "}\n\n";
 
